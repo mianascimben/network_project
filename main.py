b35db7de77295f409525636e52178a0f16a475ef
Mia Nascimben <nascimbenmia@gmail.com>
2024-06-20 10:17:39 +0200

    fix: solve issue in data acquisition
    Body: change the identificator for airports, from IATA code to Airport ID

--- main.py
+++ main.py
@@ -21,21 +21,22 @@ with zipfile.ZipFile(zip_path, 'r') as z:
     with z.open('airports.csv') as file2:
         airports=pd.read_csv(file2, delimiter=',', na_values=r'\N')
 
-#cleaning data
-routes_clean = routes.dropna(subset=['Source airport', 'Destination airport'])
-airports_clean = airports.dropna(subset = ['IATA'])
+# cleaning data: erase routes if the source or the destination is NAN
+routes_clean = routes.dropna(subset=['Source airport ID', 'Destination airport ID'])
+# cleaning data: erase all airport information if its ID is NAN
+airports_clean = airports.dropna(subset = ['Airport ID'])
 
-routes_clean2 = routes_clean.drop(routes_clean[~routes_clean['Source airport'].isin(airports_clean['IATA']) | ~routes_clean['Destination airport'].isin(airports_clean['IATA'])].index)
+routes_clean2 = routes_clean.drop(routes_clean[~routes_clean['Source airport ID'].isin(airports_clean['Airport ID']) | ~routes_clean['Destination airport ID'].isin(airports_clean['Airport ID'])].index)
 
 #creation of the dictionary for the airports positions
-air_pos = dict(zip(airports_clean['IATA'], zip(airports_clean['Longitude'], airports_clean['Latitude'])))
+air_pos = dict(zip(airports_clean['Airport ID'], zip(airports_clean['Longitude'], airports_clean['Latitude'])))
 
 # creation of a graph
-G = nx.from_pandas_edgelist(routes_clean2, source = 'Source airport', target = 'Destination airport')
+G = nx.from_pandas_edgelist(routes_clean2, source = 'Source airport ID', target = 'Destination airport ID')
 
 adj = nx.to_pandas_edgelist(G)
 adj.to_csv('flight_graph.csv', index=False)
 #visualization of the data
 fig = plt.scatter('Longitude', 'Latitude', data = airports_clean, s=8)
 
-nx.draw(G, air_pos, node_color='lightblue', edge_color='gray', node_size=10)
+nx.draw(G, air_pos, node_color='blue', edge_color='gray', node_size=10)
